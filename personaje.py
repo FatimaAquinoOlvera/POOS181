1efe32ce7afeeda2178f94c3014e753006244088
FatimaAquinoOlvera <115176954+FatimaAquinoOlvera@users.noreply.github.com>
2023-02-20 18:43:24 -0600

    Definimos metodos del personaje

--- personaje.py
+++ personaje.py
@@ -7,3 +7,20 @@ class Personaje:
     altuta= 1.90
     
     #Metodos personajes
+    
+    def correr(self,status):
+        if (status):
+            print("El personaje "+ self.nombre+ "Esta corriendo ")
+        else:
+             print("El personaje "+ self.nombre +"Se detuvo")
+             
+    def LanazarGranada(self):
+         print("Se lanzo granada")
+         
+    def RecargaArma(self, municiones):
+         cargador=5
+         cargador=cargador+municiones
+         print("El arma tiene ahora"+cargador+"balas")
+         
+         
+         
