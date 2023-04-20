738419c66a772e9ebca0de655e7a9492e3f65b9d
FatimaAquinoOlvera <115176954+FatimaAquinoOlvera@users.noreply.github.com>
2023-04-19 18:46:42 -0600

    Examen concluido

--- Examen3/controlador.py
+++ Examen3/controlador.py
@@ -8,23 +8,83 @@ class ControladorBD:
     #Metodo crear conexiones
     def conexionBD(self):
         try:
-            conexion = sqlite3.connect("BDImportaciones.db")
+            conexion = sqlite3.connect("C:/Users/Sistemas/Documents/POOS181/Examen3/BDImportaciones.db")
             print("Conectado con exito")
             return conexion
         except sqlite3.OperationalError:
             print("Fallo en la conexion")
 
-    #Metodo para ingresar mercancia
-    def ingresar(self, mercancia, pais):
+    def registar(self, mercancia, pais):
+        # Se comprueba que los campos no esten vacios
+        if (mercancia == "" or pais == ""):
+            messagebox.showwarning("CUIDADO", "Revisa tus datos, uno o mas campos estan vacios")
+            return
+
+        #Procedimiento para guardar la mercancia en la base de datos
         conx = self.conexionBD()
         cursor = conx.cursor()
         try:
+            #Se intenta guardar la mercancia
             cursor.execute("insert into TB_Europa(Mercancia, Pais) values(?,?)", (mercancia, pais))
             conx.commit()
             conx.close()
             messagebox.showinfo("EXITO", "Se guardo la mercancia")
         except:
+            #Si no se puede guardar la mercancia se muestra un mensaje de error
             messagebox.showerror("ERROR", "No se pudo guardar la mercancia")
             conx.close()
             return
 
+    def consultar(self, pais):
+        if (pais == ""):
+            messagebox.showwarning("CUIDADO", "El campo pais esta vacio")
+            return
+        #Procedimiento para consultar la mercancia en la base de datos
+        conx = self.conexionBD()
+        cursor = conx.cursor()
+        try:
+            #Se intenta consultar la mercancia
+            cursor.execute("SELECT * FROM TB_Europa WHERE Pais = ?", (pais,))
+            mercancia = cursor.fetchall()
+            conx.close()
+            # Comprobar si la mercancia existe
+            if len(mercancia) == 0:
+                # Si la mercancia no existe se retorna None y se muestra un mensaje de error
+                messagebox.showwarning("Error", "La mercancia con ese pais no existe")
+                return
+            return mercancia
+        except:
+            #Si no se puede consultar la mercancia se muestra un mensaje de error
+            messagebox.showerror("ERROR", "No se pudo consultar la mercancia")
+            conx.close()
+            return
+
+    def eliminar(self, id):
+        # Se comprueba que id no esten vacio
+        if (id == ""):
+            messagebox.showwarning("CUIDADO", "El campo id esta vacio")
+            return
+        #Procedimiento para eliminar la mercancia en la base de datos
+        conx = self.conexionBD()
+        cursor = conx.cursor()
+        try:
+            #Comprobar si la mercancia existe
+            cursor.execute("SELECT * FROM TB_Europa WHERE IDImpo = ?", (id,))
+            mercancia = cursor.fetchall()
+            if len(mercancia) == 0:
+                # Si la mercancia no existe se retorna None y se muestra un mensaje de error
+                messagebox.showwarning("Error", "La mercancia con ese id no existe")
+                conx.close()
+                return
+            #Se intenta eliminar la mercancia
+            cursor.execute("DELETE FROM TB_Europa WHERE IDImpo = ?", (id,))
+            conx.commit()
+            conx.close()
+            messagebox.showinfo("EXITO", "Se elimino la mercancia")
+        except:
+            #Si no se puede eliminar la mercancia se muestra un mensaje de error
+            messagebox.showerror("ERROR", "No se pudo eliminar la mercancia")
+            conx.close()
+            return
+
+ 
--- Examen3/ventana.py
+++ Examen3/ventana.py
@@ -1,5 +1,38 @@
 import tkinter as tk
 from tkinter import ttk
+from controlador import ControladorBD
+
+# Crear un objeto controlador clase ControladorBD
+controlador = ControladorBD()
+
+def registro():
+    mercancia = mercancia_ingresar.get()
+    pais = pais_ingresar.get()
+    try:
+        controlador.registar(mercancia, pais)
+    except:
+        print("Error al ingresar")
+
+def consultar():
+    pais = pais_consultar.get()
+    mercancia = controlador.consultar(pais)
+    if mercancia is None:
+        textoBuscar.delete(1.0, tk.END)  # Borra el contenido actual del widget de texto
+        textoBuscar.insert(tk.END, "No se encontró mercancía para ese país")
+    else:
+        textoBuscar.delete(1.0, tk.END)  # Borra el contenido actual del widget
+        #Usa un ciclo for para recorrer la lista de mercancia y mostar los datos
+        for mercancia in mercancia:
+            id = mercancia[0]
+            nombre_mercancia = mercancia[1]
+            pais_mercancia = mercancia[2]
+            textoBuscar.insert(tk.END, "\nID: " + str(id) + "\nMercancia: " + nombre_mercancia + "\nPais: " + pais_mercancia)
+
+        
+def eliminar():
+    id = id_eliminar.get()
+    controlador.eliminar(id)
+
 
 Ventana = tk.Tk()
 Ventana.title("Importaciones Europa")
@@ -16,5 +49,60 @@ notebook.add(panel2, text="Consultar")
 
 panel3 = ttk.Frame(notebook)
 notebook.add(panel3, text="Eliminar")
+
+#Panel de registro
+
+labelIngresar = tk.Label(panel1, text="Insertar mercancia", fg="green")
+labelIngresar.pack(pady=10)
+
+labelMercancia = tk.Label(panel1, text="Nombre de mercancia", fg="green")
+labelMercancia.pack(pady=10)
+mercancia_ingresar = tk.StringVar()
+entryMercancia = tk.Entry(panel1, textvariable=mercancia_ingresar)
+entryMercancia.pack(pady=5)
+
+labelPais = tk.Label(panel1, text="Pais", fg="green")
+labelPais.pack(pady=10)
+pais_ingresar = tk.StringVar()
+entryPais = tk.Entry(panel1, textvariable=pais_ingresar)
+entryPais.pack(pady=5)
+
+botonIngresar = tk.Button(panel1, text="Ingresar", command=registro, bg="green", fg="white")
+botonIngresar.pack(pady=10)
+
+
+#Panel de consulta
+
+labelConsultar = tk.Label(panel2, text="Buscar por pais", fg="blue")
+labelConsultar.pack(pady=10)
+
+labelPais = tk.Label(panel2, text="Pais", fg="blue")
+labelPais.pack(pady=10)
+pais_consultar = tk.StringVar()
+entryPais = tk.Entry(panel2, textvariable=pais_consultar)
+entryPais.pack(pady=5)
+
+botonConsultar = tk.Button(panel2, text="Consultar", command=consultar, bg="blue", fg="white")
+botonConsultar.pack(pady=10)
+
+textoBuscar = tk.Text(panel2, width=40, height=10)
+textoBuscar.pack(pady=10)
+
+
+#Panel de eliminacion
+
+labelEliminar = tk.Label(panel3, text="Eliminar por ID", fg="red")
+labelEliminar.pack(pady=10)
+
+labelID = tk.Label(panel3, text="ID", fg="red")
+labelID.pack(pady=10)
+id_eliminar = tk.StringVar()
+entryID = tk.Entry(panel3, textvariable=id_eliminar)
+entryID.pack(pady=5)
+
+botonEliminar = tk.Button(panel3, text="Eliminar", command=eliminar, bg="red", fg="white")
+botonEliminar.pack(pady=10)
+
+
 
 Ventana.mainloop()
